d4f98982cf9aeb316db33acf4480efc819f60c66
LudvinBaquiax56 <vasqueznicolas56@gmail.com>
2021-06-18 13:13:23 -0600

    Clase jugador creado

---
+++ src/Jugadores/Jugador.java
@@ -0,0 +1,56 @@
+package src.Jugadores;
+
+public class Jugador {
+
+	private String nombre;
+	private int jugados;
+	private int ganados;
+	private int perdidos;
+
+	public Jugador(String nombre){
+		this.nombre = nombre;
+		jugados = 0;
+		ganados = 0;
+		perdidos = 0;
+	}
+
+	public Jugador(String nombre, int jugados, int ganados, int perdidos){
+		this.nombre = nombre;
+		this.jugados = jugados;
+		this.ganados = ganados;
+		this.perdidos = perdidos;
+	}
+
+	public String getNombre(){
+		return nombre;
+	}
+
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
+
+	public int getJugados(){
+		return jugados;
+	}
+
+	public void setJugados(int jugados){
+		this.jugados = jugados;
+	}
+
+
+	public int getGanados(){
+		return ganados;
+	}
+
+	public void setGandos(int ganados){
+		this.ganados = ganados;
+	}
+
+	public int getPerdidos(){
+		return perdidos;
+	}
+
+	public void setPerdidos(int perdidos){
+		this.perdidos = perdidos;
+	}
+}
